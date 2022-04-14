aa3ec32f488f1ec040485bc1ee7e82db249137e1
l33th <l33th@protonmail.ch>
2022-04-14 18:10:44 +0200

    plotting data

--- main.py
+++ main.py
@@ -56,3 +56,12 @@ forecast = m.predict(future)
 
 st.subheader('Forecast data')
 st.write(forecast.tail())
+
+# Plot data
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
