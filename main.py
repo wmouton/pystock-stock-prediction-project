0aa6e4a3fa2d24790aee69f33f339c1566c816cc
l33th <l33th@protonmail.ch>
2022-04-14 17:54:13 +0200

    load data function

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("PyStock - Stock Prediction App")
 
 stocks = ("AAPL", "GOOG", "MSFT", "GME")
-selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of prediction: ", 1, 4)
 period = n_years * 365
+
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stock)
+data_load_state.text("Loading data... done!")
