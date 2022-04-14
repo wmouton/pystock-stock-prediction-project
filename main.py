0d0731f9cec85cff09082372bd28aed06fce57a2
l33th <l33th@protonmail.ch>
2022-04-14 17:36:28 +0200

    stocks and years

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("PyStock - Stock Prediction App")
+
+stocks = ("AAPL", "GOOG", "MSFT", "GME")
+selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction: ", 1, 4)
+period = n_years * 365
