6aa0328e2ac113aca98ae2cdd30e4ca7438132ec
l33th <l33th@protonmail.ch>
2022-04-16 13:06:33 +0200

    full fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,13 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("PyStock - Stock Prediction App")
+st.title("[ PyStock ] - by WMouton")
+st.title("Stock Prediction App")
 
 stocks = ("AAPL", "GOOG", "MSFT", "GME")
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
+selected_stock = st.selectbox("Select Dataset For Prediction", stocks)
 
-n_years = st.slider("Years of prediction: ", 1, 4)
+n_years = st.slider("Years Of Prediction: ", 1, 4)
 period = n_years * 365
 
 
@@ -53,14 +54,16 @@ m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
-st.subheader('Forecast data')
+st.subheader('Forecast Data')
 st.write(forecast.tail())
 
 # Plot data
-st.write('forecast data')
+st.write('Forecast Data')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.write('forecast components')
+st.write('Forecast Components')
 fig2 = m.plot_components(forecast)
 st.write(fig2)
+
+st.title("- by WMouton @ l33th")
