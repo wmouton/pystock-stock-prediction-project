107b61cc538428b7b40ed4bb43c2c5dcbdd3f5fd
l33th <l33th@protonmail.ch>
2022-04-16 12:47:22 +0200

    updated and fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 import yfinance as yf
 
 from datetime import date
-from prophet import yf
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 
